1c3df476e8a0d407efcd82e58df7f5ed30b125ca
michelangelo <michelangelocali@hotmail.com>
2022-09-23 21:17:24 -0300

    added modal without style

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,33 @@
-import { Dashboard } from "./components/Dashboard/Dashboard"
+import { useState } from "react"
+import Modal from "react-modal"
 import { Header } from "./components/Header/Header"
+import { NewTransactionModal } from "./components/NewTransactionModal/NewTransactionModal"
+import { Dashboard } from "./components/Dashboard/Dashboard"
+
 import { GlobalStyle } from "./styles/global"
 
+Modal.setAppElement('#root')
+
 export const App = () => {
+  const [isNewTransactionModalOpen, setIsNewTransactionModalOpen ] = useState(false)
+
+  const handleOpenNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(true)
+  }
+
+  const handleCloseNewTransactionModal = () => {
+    setIsNewTransactionModalOpen(false)
+  }
+
   return (
     <>
-      <Header />
+      <Header 
+        onOpenNewTransactionModal={handleOpenNewTransactionModal}
+      />
+      <NewTransactionModal 
+        isOpen={isNewTransactionModalOpen} 
+        onRequestClose={handleCloseNewTransactionModal}  
+      />
       <Dashboard />
       <GlobalStyle/>
     </>
